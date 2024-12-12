1de1a399ababfd2298e96dd7819d1a3e1677d589
gabri32 <gabodelac@gmail.com>
2024-12-11 23:45:21 -0500

    corsss echos para produccion

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const conectarDB = require('./config/db');
 const preventaRoutes = require('./routes/preventaRoutes');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+
+
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    console.log("datos del origin",origin)
+    if (origin && allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No autorizado por CORS'));
+    }
+  }
+}));
 
 // Middleware para manejar el cuerpo de las solicitudes
 app.use(express.json());
